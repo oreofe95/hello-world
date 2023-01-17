0b472ed08a0cef21514d5f907f4f7858cbbf34a3
babz <dewalebabz@gmail.com>
2023-01-17 20:22:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_network_interface" "foo" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  ami           = "ami-0b5eea76982371e91" # us-west-2
   instance_type = "t2.micro"
 
   network_interface {
